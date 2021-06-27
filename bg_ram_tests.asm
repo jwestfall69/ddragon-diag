7ff719f893eb4d2b3539159de35c240550f75aea
jwestfall <jwestfall@surrealistic.net>
2021-06-27 15:51:20 -0700

    add manual bg ram tests

--- bg_ram_tests.asm
+++ bg_ram_tests.asm
@@ -6,6 +6,7 @@
 	global auto_bg_ram_tests
 	global manual_bg_ram_tests
 	global STR_TESTING_BG_RAM
+	global STR_BG_RAM_TESTS
 
 	section text
 
@@ -35,7 +36,112 @@ auto_bg_ram_tests:
 		rts
 
 manual_bg_ram_tests:
+
+		FG_XY	10,4
+		ldy	#STR_BG_RAM_TESTS
+		JRU	fg_print_string
+
+		FG_XY	2,12
+		ldy	#STR_PASSES
+		JRU	fg_print_string
+
+		FG_XY	5,25
+		ldy	#STR_A_PAUSE
+		JRU	fg_print_string
+
+		FG_XY	5,26
+		ldy	#STR_C_MAIN_MENU
+		JRU	fg_print_string
+
+		ldw	#0		; # of passes
+		pshsw
+
+	.loop_next_test:
+
+		pulsw
+		tfr	w,d
+		pshsw
+
+		FG_XY	12,12
+		JRU	fg_print_hex_word
+
+		ldd	#BG_RAM_START
+		std	g_mt_mem_start_address
+		ldd	#BG_RAM_SIZE
+		std	g_mt_mem_size
+		lda	#BG_RAM_ADDRESS_LINES
+		sta	g_mt_mem_address_lines
+		lda	#$ff
+		sta	g_mt_data_mask
+
+		jsr	mem_tester
+		tsta
+		bne	.test_failed
+
+		pulsw
+		incw
+		pshsw
+
+		jsr	input_update
+		lda	g_p1_input_edge
+
+		bita	#A_BUTTON
+		beq	.a_not_pressed
+		jsr	bg_clear
+		jsr	wait_a_release
+
+	.a_not_pressed:
+
+		lda	g_extra_input_edge
+		bita	#C_P1_BUTTON
+		beq	.loop_next_test
+
+		; fix up stack
+		pulsw
+
+		jsr	bg_clear
+		rts
+
+	.test_failed:
+		adda	#(EC_BG_RAM_DEAD_OUTPUT - 1)
+
+		; clear our bg ram to fix the screen
+		pshs	a
+		jsr	bg_clear
+		puls	a
+
+		jsr	print_error
+
+		FG_XY	10,4
+		ldy	#STR_BG_RAM_TESTS
+		JRU	fg_print_string
+
+		FG_XY	0,12
+		ldy	#STR_PASSES
+		JRU	fg_print_string
+
+		pulsw
+		tfr	w,d
+		FG_XY	12,12
+		JRU	fg_print_hex_word
+
+		STALL
+		rts
+
+wait_a_release:
+		lda	INPUT_P1
+		anda	#A_BUTTON
+		beq	wait_a_release
+		rts
+
+bg_clear:
+		ldx	#BG_RAM_START
+		ldw	#BG_RAM_SIZE
+		clra
+		JRU	ram_fill
 		rts
 
 STR_TESTING_BG_RAM:		string "TESTING BG RAM"
+STR_BG_RAM_TESTS:		string "BG RAM TESTS"
 
+STR_A_PAUSE:			string "A - HOLD TO PAUSE"
--- ddragon_diag.asm
+++ ddragon_diag.asm
@@ -173,6 +173,7 @@ automatic_tests:
 ;}
 
 MAIN_MENU_ITEMS_START:
+	MAIN_MENU_ITEM STR_BG_RAM_TESTS, manual_bg_ram_tests
 	MAIN_MENU_ITEM STR_INPUT_TESTS, manual_input_tests
 	MAIN_MENU_ITEM STR_SOUND_TESTS, manual_sound_tests
 	MAIN_MENU_ITEM STR_VIDEO_DAC_TESTS, manual_video_dac_tests
